d162c85060f2e233e81f19bc7822a5a6076b6a4b
Deepak Deodatt Mishra <dt1702281@gmail.com>
2026-06-11 05:23:24 +0530

    hash update

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,11 +1,12 @@
 import { StrictMode } from 'react'
+import { HashRouter } from "react-router-dom";
 import { BrowserRouter } from "react-router-dom";
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
 
 createRoot(document.getElementById('root')).render(
-  <BrowserRouter>
+  <HashRouter>
     <App />
-  </BrowserRouter>
+  </HashRouter>
 )

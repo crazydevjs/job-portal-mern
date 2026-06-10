6c61dd1704e801e81582ffb5d2a712f07ca468f0
Deepak Deodatt Mishra <dt1702281@gmail.com>
2026-06-11 04:45:04 +0530

    add register page

--- frontend/src/pages/Register.jsx
+++ frontend/src/pages/Register.jsx
@@ -11,7 +11,7 @@ function Register() {
 
     try {
       await axios.post(
-        "http://localhost:5000/api/users/register",
+        "https://job-portal-mern-88c6.onrender.com/api/users/register",
         {
           name,
           email,

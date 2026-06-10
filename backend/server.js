ad05ef123d08afc1818c6d35dc6da1f019233345
Deepak Deodatt Mishra <dt1702281@gmail.com>
2026-06-11 03:32:37 +0530

    api url

--- backend/server.js
+++ backend/server.js
@@ -3,7 +3,7 @@ const cors = require("cors");
 require("dotenv").config();
 
 
-const connectDB = require("./config/db");
+const connectDB = require("./config/db.js");
 
 const app = express();
 
--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -4,7 +4,7 @@ import api from "../services/api";
 const res = await api.get("/jobs");
 
 const api = axios.create({
-  baseURL: "http://localhost:5000/api",
+  baseURL: "https://job-portal-mern-88c6.onrender.com/api",
 });
 
 export default api;
